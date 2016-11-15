b8c29cf3ac2f38d19aad849b728c094722ebc7de
RichardMorris750 <richard.morris@rcp-consultants.co.uk>
2016-11-15 15:45:48 +0000

    Upgraded to Symphony 3.2.9 Added drill method and field size attributes to the product group field in the product structure. Added a new Product Grpoup lookup capability to the SFE_UI library.  This allows it to be used throughout the system. Added the product group lookup view model to the product view model and the lookup list to the product maintenance screen/visual states. Added ReGen.bat to generate some additional files.

--- ReGen.bat
+++ ReGen.bat
@@ -13,5 +13,8 @@ set OPTS=-e -r
 rem Data library
 codegen %OPTS% -s SUPPLIER PRODUCT_GROUP -t Symphony_DOCache_v3_3.tpl -o %ROOT%\AppData_Server -n AppData -ut MODELNAMESPACE=AppData -ut ASSEMBLYNAME=AppData
 
+codegen %OPTS% -s PRODUCT -t Symphony_Style_v3_2 -o %ROOT%SFE_UI\Resources -n SFE_UI.Resources -ut ASSEMBLYNAME=SFE_UI
+
+
 endlocal
 

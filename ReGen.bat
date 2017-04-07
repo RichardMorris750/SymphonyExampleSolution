af71146ebe2c49553e50c214f44efcd05a19bbf3
Allan Shikhvarg <allan.shikhvarg@synergex.com>
2017-04-07 12:40:07 -0400

    Added Buyer Structure to Repository & created applicable objects via CodeGen

--- ReGen.bat
+++ ReGen.bat
@@ -11,9 +11,22 @@ set RPSTFIL=%ROOT%Rps\rpstext.ism
 set OPTS=-e -r
 
 rem Data library
-codegen %OPTS% -s SUPPLIER PRODUCT_GROUP -t Symphony_DOCache_v3_3.tpl -o %ROOT%\AppData_Server -n AppData -ut MODELNAMESPACE=AppData -ut ASSEMBLYNAME=AppData
+codegen %OPTS% -s BUYER -t Symphony_Data_v3_2.tpl 	-o %ROOT%\AppData_Server -n AppData -ut ASSEMBLYNAME=AppData
+codegen %OPTS% -s BUYER -t Symphony_DataFieldMapper.tpl -o %ROOT%\AppData_Server -n AppData -ut ASSEMBLYNAME=AppData
+codegen %OPTS% -s BUYER -t Symphony_FileIO.tpl 		-o %ROOT%\AppData_Server -n AppData -ut ASSEMBLYNAME=AppData
+codegen %OPTS% -s BUYER -t Symphony_DOCache_v3_3.tpl 	-o %ROOT%\AppData_Server -n AppData -ut MODELNAMESPACE=AppData -ut ASSEMBLYNAME=AppData
 
-codegen %OPTS% -s PRODUCT -t Symphony_Style_v3_2 -o %ROOT%SFE_UI\Resources -n SFE_UI.Resources -ut ASSEMBLYNAME=SFE_UI
+codegen %OPTS% -s BUYER -t Symphony_Collection_v3_2.tpl -o %ROOT%\SFE_UI\Content -n SFE_UI.Content
+
+codegen %OPTS% -s BUYER -t Symphony_Content.tpl 	-o %ROOT%SFE_UI\Resources -n SFE_UI.Resources -ut ASSEMBLYNAME=SFE_UI
+codegen %OPTS% -s BUYER -t Symphony_Style_v3_2 		-o %ROOT%SFE_UI\Resources -n SFE_UI.Resources -ut ASSEMBLYNAME=SFE_UI
+
+
+
+
+
+
+
 
 
 endlocal

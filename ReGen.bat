e0616f05f86448a85e83acb176071af37d3467b1
Richard C. Morris <richard.morris@rcp-consultants.co.uk>
2019-04-10 10:50:45 +0100

    Added a very simple Infragistics example.

--- ReGen.bat
+++ ReGen.bat
@@ -21,13 +21,24 @@ set clientProjectNameSpace=SFE_UI
 set OPTS=-e -r
 
 rem Data library
-codegen %OPTS% -s %strList% -t Symphony_Data_V3_2 -o %ROOT%\AppData_Server -n %serverProjectNameSpace%
+codegen %OPTS% -s %strList% -t Symphony_Data_V3_2 -o %ROOT%\%serverProjectNameSpace%_Server -n %serverProjectNameSpace%
 
 rem Stadnard File IO classes
-codegen %OPTS% -s %strListLessParameter% -t Symphony_FileIO_V3_2 -o %ROOT%\AppData_Server -n AppData -ut MODELNAMESPACE=%serverProjectNameSpace%
+codegen %OPTS% -s %strListLessParameter% -t Symphony_FileIO_V3_2 -o %ROOT%\%serverProjectNameSpace%_Server -n %serverProjectNameSpace% -ut MODELNAMESPACE=%serverProjectNameSpace%
+
+rem C# CLR version
+rem Data library
+codegen %OPTS% -s %strList% -t Symphony_Data_V3_2 -o %ROOT%\%serverProjectNameSpace%_CLR -n %serverProjectNameSpace%
+
+rem Stadnard File IO classes
+codegen %OPTS% -s %strListLessParameter% -t Symphony_FileIO_V3_2 -o %ROOT%\%serverProjectNameSpace%_CLR -n AppData -ut MODELNAMESPACE=%serverProjectNameSpace%
 
 rem Data object cache classes
-codegen %OPTS% -s BUYER SUPPLIER PRODUCT_GROUP -t Symphony_DOCache_v3_3.tpl 	-o %ROOT%%serverProjectNameSpace%_Server -n %serverProjectNameSpace% -ut MODELNAMESPACE=%serverProjectNameSpace% -ut ASSEMBLYNAME=%serverProjectNameSpace%_Server
+codegen %OPTS% -s BUYER SUPPLIER PRODUCT_GROUP -t Symphony_DOCache_v3_3.tpl -o %ROOT%%serverProjectNameSpace%_Server -n %serverProjectNameSpace% -ut MODELNAMESPACE=%serverProjectNameSpace% -ut ASSEMBLYNAME=%serverProjectNameSpace%_Server
+
+rem C# CLR version
+rem Data object cache classes
+codegen %OPTS% -s BUYER SUPPLIER PRODUCT_GROUP -t Symphony_DOCache_v3_3.tpl -o %ROOT%%serverProjectNameSpace%_CLR -n %serverProjectNameSpace% -ut MODELNAMESPACE=%serverProjectNameSpace% -ut ASSEMBLYNAME=%serverProjectNameSpace%_CLR
 
 rem Data mapping
 codegen %OPTS% -s %strList% -t Symphony_DataFieldMapper -o %ROOT%\%serverProjectNameSpace%_Server -n %serverProjectNameSpace%
@@ -45,8 +56,17 @@ rem Content files contain repository selection field data
 codegen %OPTS% -s %strList% -t Symphony_Content -o %ROOT%\%clientProjectNameSpace%\Resources -n %clientProjectNameSpace%.Content -ut ASSEMBLYNAME=%clientProjectNameSpace%
 
 rem Styles define field input control styling
-REM codegen %OPTS% -s %strList% -t Symphony_Style_V3_2 -o %ROOT%\%clientProjectNameSpace%\Resources -n %serverProjectNameSpace% -ut ASSEMBLYNAME=SFE_UI
 codegen %OPTS% -s %strList% -t Symphony_Style_SFE -o %ROOT%\%clientProjectNameSpace%\Resources -n %serverProjectNameSpace% -ut ASSEMBLYNAME=SFE_UI
+
+
+rem C# CLR/IG version
+rem Content files contain repository selection field data
+codegen %OPTS% -s %strList% -t Symphony_Content -o %ROOT%\%clientProjectNameSpace%_IG\Resources -n %clientProjectNameSpace%.Content -ut ASSEMBLYNAME=%clientProjectNameSpace%
+
+rem Styles define field input control styling
+codegen %OPTS% -s %strList% -t Symphony_Style_IG_V3_2 -o %ROOT%\%clientProjectNameSpace%_IG\Resources -n %serverProjectNameSpace% -ut ASSEMBLYNAME=SFE_UI
+
+
 
 rem Create the connection object between styled input controls and select data 
 codegen %OPTS% -s %strList% -t Symphony_Collection_V3_2 -o %ROOT%\%clientProjectNameSpace%\Content -n %clientProjectNameSpace%.Content -ut ASSEMBLYNAME=%clientProjectNameSpace%
@@ -64,6 +84,8 @@ codegen e -r -s BUYER -t Symphony_SPA_MaintLogic_v3_2 -o "%ROOT%SFE_Web\Scripts"
 codegen e -r -s SUPPLIER -t Symphony_SPA_MaintLogic_v3_2 -o "%ROOT%SFE_Web\Scripts" -ut RESTRICTEDFIELDLIST="supp_code,name"
 codegen e -r -s PRODUCT_GROUP -t Symphony_SPA_MaintLogic_v3_2 -o "%ROOT%SFE_Web\Scripts" -ut RESTRICTEDFIELDLIST="prod_group,group_desc"
 codegen e -r -s CUSTOMER -t Symphony_SPA_MaintLogic_v3_2 -o "%ROOT%SFE_Web\Scripts" -ut RESTRICTEDFIELDLIST="cust_code,name"
+
+:theEnd
 
 endlocal
 
